9c0998a371bf7b40eda7c8d5c39b3d865be8bafb
bandam <bandam@adobe.com>
2021-02-22 17:24:09 +0530

    First Commit

---
+++ src/test/java/TestPackage/Testprgm.java
@@ -0,0 +1,12 @@
+package TestPackage;
+import org.testng.annotations.Test;
+
+public class Testprgm 
+{
+	
+@Test
+	public void hellotest()
+	{
+		System.out.println("Maven Program execution Successfull");
+	}
+}
